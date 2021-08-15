4261a72239aa913258040e121a0bc8b96e406361
ncc-74205 <ncc-74205@exospectrum.com>
2021-08-15 14:40:51 +0200

    test8

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
+oledssd1306.clearDisplay()
+oledssd1306.setTextXY(0, 0)
+oledssd1306.writeString("Hello, world.")
+
 // tests go here; this will not be compiled when this package is used as a library
